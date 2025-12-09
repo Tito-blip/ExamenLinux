1a669fb4f552ca6e4b15656ad9c945040184735a
Tito-blip <122133993+Tito-blip@users.noreply.github.com>
2025-12-09 15:48:07 -0400

    Enhance setup script with comments and structure
    Updated comments and improved script readability.

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt update # Actualizar paquetes apt
 
 echo "Paquetes actualizados correctamente."
 
-PACKAGES=("git" "curl" "ufw" "docker-compose")
-for p in ${PACKAGES[@]}; do
+PACKAGES=("git" "curl" "ufw" "docker-compose") # Lista de paquetes para instalar
+
+for p in ${PACKAGES[@]}; do # Instalar paquetes con apt
 	echo "Instalando $p..."
 	sudo apt install -y $p
 	echo "$p Instalado correctamente"
@@ -13,6 +14,7 @@ done
 
 DIR="/opt/webapp/html"
 
+# Crear usuario sysadmin y grupo docker
 if getent group "docker" > /dev/null; then
 	echo "Grupo 'docker' ya existe"
 else
@@ -27,7 +29,7 @@ else
 	echo "Usuario 'sysadmin' creado y asignado al grupo 'docker'"
 fi
 
-if [ ! -d $DIR ]; then
+if [ ! -d $DIR ]; then # Crear carpeta para webapp y cambiar permisos para sysadmin
 	echo "Creando carpeta en $DIR..."
 	sudo mkdir -p $DIR
 	sudo chgrp -R docker $DIR
@@ -36,26 +38,24 @@ if [ ! -d $DIR ]; then
 else
 	cd "$DIR" || echo "Error al acceder al directorio $DIR"
 	echo "Directorio actual: $PWD"
-	if curl -sS -O https://gist.githubusercontent.com/DarkestAbed/0c1cee748bb9e3b22f89efe1933bf125/raw/5801164c0a6e4df7d8ced00122c76895997127a2/docker-compose.yml; then
+fi
+
+# Descargar archivo docker-compose.yml
+if curl -sS -O https://gist.githubusercontent.com/DarkestAbed/0c1cee748bb9e3b22f89efe1933bf125/raw/5801164c0a6e4df7d8ced00122c76895997127a2/docker-compose.yml; then
         echo "docker-compose.yml descargado"
-    else
+else
         echo "Error al descargar docker-compose.yml"
-    fi
-	if [ ! -e index.html ]; then
+fi
+
+# Crear archivo html para nginx
+if [ ! -e index.html ]; then
 		touch index.html
 		sudo tee index.html > /dev/null << 'EOF'
-		<!DOCTYPE html>
-		<html lang="es">
-			<head>
-    			<meta charset="UTF-8">
-    			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    			<title>WebApp</title>
-			</head>
-			<body>
-    			<h1>Servidor Seguro Propiedad de [Benjamin Gonzalez] - Acceso Restringido</h1>
-			</body>
-		</html>
+    			<h1>Servidor Seguro Propiedad de Benjamin Gonzalez - Acceso Restringido</h1>
 EOF
-	fi
-	sudo chown sysadmin:docker /opt/webapp/html/*
 fi
+
+cd $DIR
+sudo mv index.html $DIR/html # Necesario para no modificar compose inicial, manteniendo la integridad original (volumen de nginx).
+sudo docker-compose up -d # Iniciar contenedor nginx con archivo compose.yml
+sudo chown sysadmin:docker /opt/webapp/html/* # Cambiar permisos para usuario sysadmin y grupo docker
